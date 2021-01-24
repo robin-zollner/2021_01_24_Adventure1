c0712c9e4bdfa8b50c39728730a05f4ed1e55a7f
Robin-zollner <robin.zollner97@gmail.com>
2021-01-24 15:29:28 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+def intro():
+    print("Hello Adventurer!)")
+    print("What shall I call you?")
+    user_name = input()
+    print("Hello {}!".format(user_name))
+    print("Welcome to the Hueville!")
+
+intro()
